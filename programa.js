654ae20d590a797cf94de7f530537cc292b5b9bd
ElMazaa <og.gripexx@gmail.com>
2022-10-24 17:46:50 -0300

    arreglos en faltas de ortografia

--- programa.js
+++ programa.js
@@ -1,13 +1,13 @@
 /*Programa que determina que tipo de usuario eres y si eres mayor de edad como para poder acceder a el sitio
 */
-let nombre = prompt("Buenas!! corroboraremos unos datos para verificar tu identidad, como dato importante debes de escribir todo con minusculas comenzemos... (Ingresa tu nombre)");
+let nombre = prompt("Buenas!! corroboraremos unos datos para verificar tu identidad, como dato importante debes de escribir todo con minúsculas comencemos... (Ingresa tu nombre)");
 let apellido = prompt("Ingresa tu apellido")
-let cedula = prompt("Ingrea tu cecula de identidad sin puntos ni giones CI:");
+let cedula = prompt("Ingresa tu cédula de identidad sin puntos ni guiones CI:");
 const edad = Number(prompt("Por ultimo ingresa tu edad"));
-let Categorias = prompt("¿Qué categoria de usuario tienes?\n1-usuario\n2-administrador\n3-dueño\n4-seo\n5-pasante");
+let Categorias = prompt("¿Qué categoría de usuario tienes?\n1-usuario\n2-administrador\n3-dueño\n4-seo\n5-pasante");
 
 if (edad >= 18) {
-	alert("Todo esta listo para que puedas trabajar buena jornada.");
+	alert("Todo está listo para que puedas trabajar buena jornada.");
 
 
 	switch (Categorias) {
@@ -16,7 +16,7 @@ if (edad >= 18) {
 			break;
 
 		case "2":
-			alert("Tienes permisos de administraor.");
+			alert("Tienes permisos de administrador.");
 			break;
 
 		case "3":
@@ -24,15 +24,15 @@ if (edad >= 18) {
 			break;
 
 		case "4":
-			alert("Tienes permisos de seo");
+			alert("Tienes permisos de seo.");
 			break;
 		case "5":
-			alert("Tienes permisos de pasante");
+			alert("Tienes permisos de pasante.");
 			break;
 
 		default:
-			alert("Ese tipo de usuario no existe.  lo siento ya no podemos continuar intentalo nuevamente.");
+			alert("Ese tipo de usuario no existe.  lo siento ya no podemos continuar inténtalo nuevamente.");
 	}
 } else {
-	alert("bienvenido pasante de ingeniero tendremos todo listo para que peudas trabajar con tu ingeniero en jefe");
+	alert("Bienvenido pasante de ingeniero tendremos todo listo para que puedas trabajar con tu ingeniero en jefe");
 }
