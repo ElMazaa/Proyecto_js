61a526d5713d615e7ee80379183d4baca5112565
ElMazaa <og.gripexx@gmail.com>
2022-10-31 18:39:35 -0300

    reentrega con funciones

--- programa.js
+++ programa.js
@@ -1,38 +1,79 @@
 /*Programa que determina que tipo de usuario eres y si eres mayor de edad como para poder acceder a el sitio
 */
-let nombre = prompt("Buenas!! corroboraremos unos datos para verificar tu identidad, como dato importante debes de escribir todo con minúsculas comencemos... (Ingresa tu nombre)");
+
+let nombre = prompt("Buenas!! Corroboraremos unos datos para verificar tu identidad, como dato importante debes de escribir todo con minúsculas comencemos... (Ingresa tu nombre)");
 let apellido = prompt("Ingresa tu apellido")
-let cedula = prompt("Ingresa tu cédula de identidad sin puntos ni guiones CI:");
-const edad = Number(prompt("Por ultimo ingresa tu edad"));
-let Categorias = prompt("¿Qué categoría de usuario tienes?\n1-usuario\n2-administrador\n3-dueño\n4-seo\n5-pasante");
 
-if (edad >= 18) {
-	alert("Todo está listo para que puedas trabajar buena jornada.");
+comprobarLaedad()
+categorias()
 
+
+//Verificar la edad de el usuario
+function comprobarLaedad() {
+	let soloSiesmayor = confirm("Solo pueden entrar usuarios mayores de edad")
+	ingreseLaedad(soloSiesmayor)
+}
+
+
+function ingreseLaedad(soloSiesmayor) {
+	if (soloSiesmayor) {
+		let edad = Number(prompt("Ingresa tu edad aqui"))
+
+		confirmarSuedad(edad)
+	} else {
+		alert("necesitamos verificar su edad")
+	}
+
+
+
+}
+
+function confirmarSuedad(edad) {
+
+	if (edad >= 18) {
+
+	} else {
+		alert("No puedes ingresar eres menor de edad")
+		comprobarLaedad()
+	}
+
+}
+
+
+
+
+// Bucle con el cual segun la categoria de el usuario sabremos que tipo de usuario es
+function categorias() {
+
+	let Categorias = prompt("¿Qué categoría de usuario tienes?\n1-usuario\n2-administrador\n3-dueño\n4-seo\n5-pasante");
 
 	switch (Categorias) {
 		case "1":
-			alert("Tienes permisos de usuario.");
+			alert("Todo está listo para que puedas trabajar buena jornadan.\nTienes permisos de usuario.");
 			break;
 
 		case "2":
-			alert("Tienes permisos de administrador.");
+			alert("Todo está listo para que puedas trabajar buena jornadan.\nTienes permisos de administraor.");
 			break;
 
 		case "3":
-			alert("Tienes permisos de dueño.");
+			alert("Todo está listo para que puedas trabajar buena jornadan.\nTienes permisos de dueño.");
 			break;
 
 		case "4":
-			alert("Tienes permisos de seo.");
-			break;
-		case "5":
-			alert("Tienes permisos de pasante.");
+			alert("Todo está listo para que puedas trabajar buena jornadan.\nTienes permisos de seo");
 			break;
 
+		case "5":
+			alert("Todo está listo para que puedas trabajar buena jornadan.\nTienes permisos de Pasante");
+			break;
+
+
 		default:
-			alert("Ese tipo de usuario no existe.  lo siento ya no podemos continuar inténtalo nuevamente.");
+			alert(" Ese tipo de usuario no existe.  lo siento ya no podemos continuar intentalo nuevamente.");
+
+
 	}
-} else {
-	alert("Bienvenido pasante de ingeniero tendremos todo listo para que puedas trabajar con tu ingeniero en jefe");
+
+
 }
